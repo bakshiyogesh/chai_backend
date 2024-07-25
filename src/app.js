faa601a09d940d8cdcdda211b211505baf91a54e
bakshiyogesh <yogeshbakshi589@gmail.com>
2024-07-25 14:32:30 +0530

    feat:added new routes for user controller

--- src/app.js
+++ src/app.js
@@ -1,31 +1,52 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
 import cookieParser from "cookie-parser";
-const app=express();
+// import cluster from 'cluster';
+// import os from 'os';
+const app = express();
+// const totalsCpu=os.cpus().length;
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
-}))
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
 
-app.use(express.json({
-    limit:"16kb"
-}))
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
 
-app.use(express.urlencoded({
-    extended:true,
-    limit:'16kb'}))
+app.use(express.static("public"));
 
-app.use(express.static("public"))
+app.use(cookieParser());
 
-app.use(cookieParser())
+// app.set("views engine","ejs"); templates eg for server side rendering
+// app.set("views",path.resolve("./views"))
 
+// clusters used for balancing the server load across same type servers.
+//  if (cluster.isPrimary) {
+//     for (let index = 0; index < totalsCpu; index++) {
+
+//         cluster.fork();
+//     }
+
+//  }else{
+//     const app= express();
+//  }
 
 //routes
 
-import userRouter from './routes/user.routes.js'
-
+import userRouter from "./routes/user.routes.js";
 
 //routes declaration
-app.use("/api/v1/users",userRouter)
-export {app};
+app.use("/api/v1/users", userRouter);
+export { app };
